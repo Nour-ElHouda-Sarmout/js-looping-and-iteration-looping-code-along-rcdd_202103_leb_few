fe18cc69b73ebca53623ea8ce3bceca109977d4c
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-15 20:49:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function writeCards(array, str){
+  let thanks = [];
+  for (let i = 0; i < array.length; i++) {
+    thanks.push(`Thank you, ${array[i]}, for the wonderful ${str} gift!`);
+}
+return thanks;
+}
